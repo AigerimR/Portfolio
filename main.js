ad43536f503c50d514d0bc724cb0c8b995c64512
aigerimr <raisovaaygerim@mail.ru>
2024-11-03 18:35:44 +0600

    new index

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-function animateWaypoint() {
-  var atarget = $(".animation");
-  atarget.each(function (idx, el) {
-      $(el).waypoint(
-          function (direction) {
-              if (direction === "down") {
-                  $(el).addClass("animated");
-              }
-          },
-          {
-              offset: "98%",
-          }
-      );
-  });
+function initAnim() {
+    $('.intro').addClass('enhanced');
+}
+function openVideo() {
+    $('.project_card_content').click(function () {
+        $(this).addClass("opened");
+        var videoSrc = $(this).data('video-src');
+        console.log(videoSrc)
+        $('.video').attr('src', videoSrc);
+    });
 }
 
+function animateSlideIn() {
+    const atargets = document.querySelectorAll(".animation");
+    const options = {
+        root: null,
+        threshold: 0.15,
+        rootMargin: "0px 0px -15% 0px"
+    };
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animated");
+            } else {
+                entry.target.classList.remove("animated");
+            }
+        });
+    }, options);
+    atargets.forEach(target => observer.observe(target));
+}
 $(function () {
-  animateWaypoint();
-})
+    initAnim();
+    openVideo();
+    animateSlideIn();
+});
+
